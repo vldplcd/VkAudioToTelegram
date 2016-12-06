a7c90bb2568f4b0e59252195e029cec18d790c3c
Антон <@AZELINSKIY>
2016-12-06 21:12:51 +0300

    Response and partly request templates added

---
+++ VK_Audio_Bot/VK_Audio_Bot/DTO/Response/Item.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VK_Audio_Bot.DTO.Response
+{
+    class Item
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("artist")]
+        public string Artist { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("duration")]
+        public int Duration { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("lyrics_id")]
+        public int Lyrics_id { get; set; }
+    }
+}
