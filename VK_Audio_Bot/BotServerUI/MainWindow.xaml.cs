d2832e340765cc44c05c524d7e011f0b9b6c5fb2
Антон Зелинский <ayuzelinskiy@edu.hse.ru>
2016-12-07 14:30:39 +0300

    UI developed

--- VK_Audio_Bot/BotServerUI/MainWindow.xaml.cs
+++ VK_Audio_Bot/BotServerUI/MainWindow.xaml.cs
@@ -1,30 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Telegram.Bot.Exceptions;
 
 namespace BotServerUI
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    /// <summary> 
+    /// Interaction logic for MainWindow.xaml 
+    /// </summary> 
     public partial class MainWindow : Window
     {
+        BotManager botm;
+
         public MainWindow()
         {
-            InitializeComponent();            
-        }       
+            InitializeComponent();
+            botm = new BotManager();
+            Dispatcher.Invoke(() => log_box.AppendText(""));
+            botm.logevent += AppendLog;
+            txb_messege.Text = "";
+        }
+
+        private void AppendLog(string log)
+        {
+            Dispatcher.Invoke(() => log_box.AppendText(log));
+        }
+
+        private async void btn_start_bot_Click(object sender, RoutedEventArgs e)
+        {
+            
+            await botm.StartBot();
+            Dictionary<long, string> usernames = new Dictionary<long, string>();
+            await Task.Run(() => { usernames = botm.Usernames().Result; });
+
+            Binding cmb_us_binding = new Binding();
+            cmb_us_binding.Source = usernames;
+            cmb_users.SetBinding(ItemsControl.ItemsSourceProperty, cmb_us_binding);
+        }
+
+        private void btn_stop_bot_Click(object sender, RoutedEventArgs e)
+        {
+            botm.StopBot();
+        }
+
+        private async void btn_send_Click(object sender, RoutedEventArgs e)
+        {
+            switch ((long)cmb_users.SelectedValue)
+            {
+                case 0:
+                    if (cmb_users.Items.Count != 0)
+                    {
+                        var chIDs = ((Dictionary<long, string>)cmb_users.ItemsSource);
+                        foreach (var c in chIDs.Keys)
+                        {
+                            if (c > 0)
+                                try
+                                {
+                                    await botm.SendMessage(c, txb_messege.Text);
+                                }
+                                catch (Exception ex)
+                                {
+                                    AppendLog($"\nFailed to send message to {c}. {ex.Message}");
+                                }
+                        }
+                    }
+                    break;
+
+                default:
+                    try
+                    {
+                        await botm.SendMessage((long)cmb_users.SelectedValue, txb_messege.Text);
+                    }
+                    catch (Exception ex)
+                    {
+                        AppendLog($"\nFailed to send message to {(long)cmb_users.SelectedValue}. {ex.Message}");
+                    }
+                    break;
+            }
+            txb_messege.Text = "";
+        }
     }
 }
