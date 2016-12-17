cb02e39f88c02b72013fbbde249a870c0216b963
Антон <@AZELINSKIY>
2016-12-17 16:33:23 +0300

    Minor fixes

--- VK_Audio_Bot/VKAudioInfoGetter/Mp3CCInfoGetter.cs
+++ VK_Audio_Bot/VKAudioInfoGetter/Mp3CCInfoGetter.cs
@@ -21,22 +21,29 @@ namespace VKAudioInfoGetter
             using (WebClient webClient = new WebClient())
             {
                 webClient.Encoding = Encoding.UTF8;
-                var htmlResult = webClient.DownloadString($"http://mp3.cc/search/f/{requestText}/");
-                var doc = new HtmlAgilityPack.HtmlDocument();
-                doc.LoadHtml(htmlResult);
-
-                var music = doc.DocumentNode.SelectSingleNode("//ul[@class='playlist']");
-
-                foreach (var li in music.SelectNodes("//li[@class='track']"))
+                try
                 {
-                    result.Add(new AudioInfo
+                    var htmlResult = webClient.DownloadString($"http://mp3.cc/search/f/{requestText}/");
+                    var doc = new HtmlAgilityPack.HtmlDocument();
+                    doc.LoadHtml(htmlResult);
+
+                    var music = doc.DocumentNode.SelectSingleNode("//ul[@class='playlist']");
+
+                    foreach (var li in music.SelectNodes("//li[@class='track']"))
                     {
-                        Artist = li.SelectSingleNode("h2").SelectSingleNode("b").SelectSingleNode("a").InnerText.ToString().Replace("&quot;", "\""),
-                        Title = li.SelectSingleNode("h2").SelectSingleNode("em").SelectSingleNode("a").InnerText.ToString().Replace("&quot;", "\""),
-                        Id = int.Parse(li.Attributes["data-id"].Value),
-                        Url = li.Attributes["data-url_song"].Value,
-                        Duration = int.Parse(li.Attributes["data-duration"].Value) / 1000,
-                    });
+                        result.Add(new AudioInfo
+                        {
+                            Artist = li.SelectSingleNode("h2").SelectSingleNode("b").SelectSingleNode("a").InnerText.ToString().Replace("&quot;", "\""),
+                            Title = li.SelectSingleNode("h2").SelectSingleNode("em").SelectSingleNode("a").InnerText.ToString().Replace("&quot;", "\""),
+                            Id = int.Parse(li.Attributes["data-id"].Value),
+                            Url = li.Attributes["data-mp3"].Value,
+                            Duration = int.Parse(li.Attributes["data-duration"].Value) / 1000,
+                        });
+                    }
+                }
+                catch(Exception ex)
+                {
+                    throw new ArgumentNullException(ex.Message);
                 }
                 return result;
             }
